5aa2bb13a5b018f4417a82433c7233e89fafae19
unknown <mishu_leu15@yahoo.com>
2021-11-27 19:03:51 +0200

    created seed for writing data on mongo, writed logic for filtering data in mongoose

--- controllers/products.js
+++ controllers/products.js
@@ -1,9 +1,28 @@
+const Product = require('../models/product');
+
 const getAllProductsStatic = async (req, res) => {
-  res.status(200).json({ msg: 'products testing route' });
+  const products = await Product.find({});
+  res.status(200).json({ nbProducts: products.length, products });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: 'products route' });
+  const { featured, company, name } = req.query;
+
+  const queryObj = {};
+
+  if (featured) {
+    queryObj.featured = featured === 'true' ? true : false;
+  }
+
+  if (company) {
+    queryObj.company = company;
+  }
+
+  if (name) {
+    queryObj.name = { $regex: name, $options: 'i' };
+  }
+  const products = await Product.find(queryObj);
+  res.status(200).json({ nbProducts: products.length, products });
 };
 
 module.exports = {
